5e47e482de17fe9230d4718a14b3a060b06b034f
Kacper-Kondracki <43544435+Kacper-Kondracki@users.noreply.github.com>
2026-06-21 01:35:36 +0200

    Run TUI on stderr

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
+use std::io::{self, BufWriter};
+
+use ratatui::Terminal;
+use ratatui::backend::CrosstermBackend;
+use ratatui::crossterm::execute;
+use ratatui::crossterm::terminal::{
+    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+};
+
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
-    let mut terminal = ratatui::init();
+    let mut terminal = init_terminal()?;
     let result = pacopt::run_app(&mut terminal);
-    ratatui::restore();
+    restore_terminal()?;
 
-    result
+    let selected = result?;
+    println!("{}", selected.join(" "));
+
+    Ok(())
+}
+
+fn init_terminal() -> io::Result<Terminal<CrosstermBackend<BufWriter<io::Stderr>>>> {
+    enable_raw_mode()?;
+    execute!(io::stderr(), EnterAlternateScreen)?;
+
+    let backend = CrosstermBackend::new(BufWriter::new(io::stderr()));
+    Terminal::new(backend)
+}
+
+fn restore_terminal() -> io::Result<()> {
+    disable_raw_mode()?;
+    execute!(io::stderr(), LeaveAlternateScreen)?;
+
+    Ok(())
 }
